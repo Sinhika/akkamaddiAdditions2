a94b9dd6d178eeabfcd434953e0ddfc99e791530
Sinhika <sinhika@republicofnewhome.org>
2017-04-16 08:53:52 -0500

    updated arsenic ModInfo to include fusion, additionslib

--- src/main/java/akkamaddi/plugins/arsenic/ModInfo.java
+++ src/main/java/akkamaddi/plugins/arsenic/ModInfo.java
@@ -6,7 +6,8 @@ public class ModInfo
     public static final String NAME = "Simple Arsenic and Old Lace";
     public static final String VERSION = "@MODVERSION@";
     public static final String ACCEPTED_VERSIONS = "[1.10.2, 1.11.2]";
-    public static final String DEPENDENCIES = "required-after:simplecore@[1.4.5.1,)";
+    public static final String DEPENDENCIES = 
+       "required-after:simplecore@[1.4.5.1,);required-after:additionslib;required-after:fusion;after:simpleores";
     public static final String CREDITS = "Created by akkamaddi, with help from Zot201. Maintained by Sinhika";
     public static final boolean USEDEPENDENCYINFO = true;
     public static final String VERSIONURL = "https://raw.githubusercontent.com/Sinhika/akkamaddiAdditions2/master/update.json";
