9d51e3d8a47d9cac10f01ec60d896bf7c671f58a
Sinhika <sinhika@republicofnewhome.org>
2017-04-16 10:23:21 -0500

    added skeletons for SimpleCobalt and SimpleCthon

---
+++ src/main/java/akkamaddi/plugins/simplecobalt/SimpleCobalt.java
@@ -0,0 +1,58 @@
+package akkamaddi.plugins.simplecobalt;
+
+import akkamaddi.plugins.additionslib.SubModHandler;
+import alexndr.api.registry.Plugin;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = ModInfo.ID, name = ModInfo.NAME, version = ModInfo.VERSION, 
+acceptedMinecraftVersions=ModInfo.ACCEPTED_VERSIONS,
+dependencies = ModInfo.DEPENDENCIES, updateJSON=ModInfo.VERSIONURL)
+public class SimpleCobalt
+{
+    @Mod.Instance(value = ModInfo.ID)
+    public static SimpleCobalt INSTANCE;
+
+    @SidedProxy(clientSide = "akkamaddi.plugins.simplecobalt.ProxyClient", 
+                    serverSide = "akkamaddi.plugins.simplecobalt.ProxyCommon")
+    public static ProxyCommon proxy;
+
+    public static Plugin plugin = new Plugin(ModInfo.ID, ModInfo.NAME);
+
+    /**
+     * Called during the PreInit phase.
+     * @param event FMLPreInitializationEvent
+     */
+    @Mod.EventHandler
+    public void PreInit(FMLPreInitializationEvent event)
+    {
+        if (!SubModHandler.moduleEnabled(SubModHandler.MODULE_COBALT)) 
+            return;
+        
+        proxy.PreInit(event);
+    } // end PreInit()
+    
+    /**
+     * Called during the Init phase.
+     * @param event FMLInitializationEvent
+     */
+    @Mod.EventHandler
+    public void Init(FMLInitializationEvent event)
+    {
+        proxy.Init(event);
+    } // end Init()
+    
+    /**
+     * Called during the PostInit phase.
+     * @param event FMLPostIntializationEvent
+     */
+    @Mod.EventHandler
+    public void PostInit(FMLPostInitializationEvent event)
+    {
+        proxy.PostInit(event);
+   }
+         
+} // end class
---
+++ src/main/java/akkamaddi/plugins/simplecthon/ProxyClient.java
@@ -0,0 +1,25 @@
+package akkamaddi.plugins.simplecthon;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class ProxyClient extends akkamaddi.plugins.simplecthon.ProxyCommon
+{
+    @Override
+    public void PreInit(FMLPreInitializationEvent event) 
+    {
+        super.PreInit(event);
+   } // end PreInit()
+
+    @Override
+    public void Init(FMLInitializationEvent event) {
+        super.Init(event);
+    } // end Init()
+
+    @Override
+    public void PostInit(FMLPostInitializationEvent event) {
+        super.PostInit(event);
+    }
+
+} // end class
