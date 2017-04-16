9c73fde1ddefc2a0de391dbc33423b3d26502683
Sinhika <sinhika@republicofnewhome.org>
2017-04-16 10:12:55 -0500

    added GoldenGlitter skeleton

---
+++ src/main/java/akkamaddi/plugins/goldenglitter/ModInfo.java
@@ -0,0 +1,14 @@
+package akkamaddi.plugins.goldenglitter;
+
+public class ModInfo
+{
+    public static final String ID = "goldenglitter";
+    public static final String NAME = "Golden Glitter";
+    public static final String VERSION = "@MODVERSION@";
+    public static final String ACCEPTED_VERSIONS = "[1.10.2, 1.11.2]";
+    public static final String DEPENDENCIES = 
+       "required-after:simplecore@[1.4.5.1,);required-after:additionslib;required-after:fusion;after:simpleores";
+    public static final String CREDITS = "Created by akkamaddi, with help from Zot201. Maintained by Sinhika";
+    public static final boolean USEDEPENDENCYINFO = true;
+    public static final String VERSIONURL = "https://raw.githubusercontent.com/Sinhika/akkamaddiAdditions2/master/update.json";
+} // end class
