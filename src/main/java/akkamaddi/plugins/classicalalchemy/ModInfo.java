3c301f9fdce30e16b0a5978d56c558599b2a6d8c
Sinhika <sinhika@republicofnewhome.org>
2017-04-16 10:13:18 -0500

    classicalalchemy should load after arsenic

--- src/main/java/akkamaddi/plugins/classicalalchemy/ModInfo.java
+++ src/main/java/akkamaddi/plugins/classicalalchemy/ModInfo.java
@@ -7,7 +7,7 @@ public class ModInfo
     public static final String VERSION = "@MODVERSION@";
     public static final String ACCEPTED_VERSIONS = "[1.10.2, 1.11.2]";
     public static final String DEPENDENCIES = 
-       "required-after:simplecore@[1.4.5.1,);required-after:additionslib;required-after:fusion;after:simpleores";
+       "required-after:simplecore@[1.4.5.1,);required-after:additionslib;required-after:fusion;after:simpleores;after:arsenic";
     public static final String CREDITS = "Created by akkamaddi, with help from Zot201. Maintained by Sinhika";
     public static final boolean USEDEPENDENCYINFO = true;
     public static final String VERSIONURL = "https://raw.githubusercontent.com/Sinhika/akkamaddiAdditions2/master/update.json";
