302aa3346c824d7bd89821c0c45c39dc10f2baed
Sinhika <sinhika@republicofnewhome.org>
2017-04-23 10:33:25 -0500

    added skeletal Recipes class to submods

---
+++ src/main/java/akkamaddi/plugins/haditecoal/Recipes.java
@@ -0,0 +1,92 @@
+package akkamaddi.plugins.haditecoal;
+
+import alexndr.api.logger.LogHelper;
+
+/**
+ * Handles recipes for this sub-module.
+ * @author Sinhika
+ *
+ */
+public class Recipes
+{
+    /** 
+     * during pre-init phase, set up ore dictionary entries.
+     */
+    public static void preInitialize() 
+    {
+        try{
+            doOreDictRecipes(); 
+            LogHelper.verbose(ModInfo.ID, 
+                "All OreDictionary entries were added successfully.");
+        }
+        catch(Exception e) {
+            LogHelper.severe(ModInfo.ID, 
+                    "OreDictionary entries were not added successfully. This is a serious problem!"); 
+            e.printStackTrace();
+        }
+    } // end preInitialize()
+    
+    /**
+     * during load/init phase, actually define recipes.
+     */
+    public static void initialize() 
+    {
+        try {
+            doRecipes();
+            doFurnaceRecipes();
+            doFusionFurnaceRecipes();
+            LogHelper.verbose(ModInfo.ID,
+                    "All recipes were added successfully.");
+        } 
+        catch (Exception e) {
+            LogHelper.severe(ModInfo.ID,
+                            "Recipes were not added successfully. This is a serious problem!");
+            e.printStackTrace();
+        }
+    } // end initialize()
+
+    /**
+     * in post-init, do recipe stuff that depends on everything else being loaded/fully-initialized.
+     * Rare that this is actually needed.
+     */
+    public static void postInitialize()
+    {
+    } // end postInitialzie()
+    
+
+    /**
+     * register ore-dictionary entries for mod objects.
+     */
+    public static void doOreDictRecipes()
+    {
+        // TODO
+    } // end doOreDictRecipes()
+
+    /**
+     * Add crafting table recipes.
+     */
+    public static void doRecipes()
+    {
+        //Block Recipes
+        //Item Recipes
+        //Tool Recipes
+        // Armor Recipes
+    } // end doRecipes()
+
+    /**
+     * add conventional furnace recipes.
+     */
+    public static void doFurnaceRecipes()
+    {
+        // TODO
+    } // end doFurnaceRecipes()
+    
+    /**
+     * add fusion furnace recipes.
+     */
+    public static void doFusionFurnaceRecipes()
+    {
+        // TODO
+    } // end doFusionFurnaceRecipes()
+    
+} // end class
